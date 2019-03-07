e8beb1ed3da383fa77c5126474541652d5008a51
sandogeek <sandogeek@163.com>
2019-03-07 15:32:59 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-export interface Options {
+export declare interface Options {
     strict: boolean;
     storeAsString: boolean;
 }
-export function parse(option?: Options): any;
-export function stringify(value: any, replacer?: Function|Array<string>, space?: number|string): string;
+export declare function parse(option?: Options): any;
+export declare function stringify(value: any, replacer?: Function|Array<string>, space?: number|string): string;
